52434ffb9226544b23bb1f80b87f61e4717c3ea8
dsnouck <dsnouck@gmail.com>
2025-05-23 14:58:36 +0200

    Add solution

---
+++ Imagine.Models/Color.cs
@@ -0,0 +1,14 @@
+﻿namespace Imagine.Models;
+
+public class Color
+{
+	public Color(string name, string hex)
+	{
+		Name = name;
+		Hex = hex;
+	}
+
+	public string Name { get; set; }
+	public string Hex { get; set; }
+
+}
