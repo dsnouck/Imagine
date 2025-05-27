8cc9b9cb05a6725c47cf4bc324c646decfb9826e
dsnouck <dsnouck@gmail.com>
2025-05-27 22:20:51 +0200

    Configure analyzers

--- Imagine.Models/Color.cs
+++ Imagine.Models/Color.cs
@@ -1,14 +1,3 @@
 ﻿namespace Imagine.Models;
 
-public class Color
-{
-	public Color(string name, string hex)
-	{
-		Name = name;
-		Hex = hex;
-	}
-
-	public string Name { get; set; }
-	public string Hex { get; set; }
-
-}
+public readonly record struct Color(double R, double G, double B);
