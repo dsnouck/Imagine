556887b597df07a1607f99b917d596b034da15bc
dsnouck <dsnouck@gmail.com>
2026-03-20 19:10:36 +0100

    Rename dotProductNormalDirection to normalDotRayDirection

--- Imagine.Scenes/Plane.cs
+++ Imagine.Scenes/Plane.cs
@@ -9,17 +9,16 @@ internal class Plane(Vector3 normal) : IScene
 
 	public List<Intercept> Intercepts(Line3 ray)
 	{
-		// TODO: Rename names using dotProduct.
-		var dotProductNormalDirection = normal.Dot(ray.Direction);
+		var normalDotRayDirection = normal.Dot(ray.Direction);
 
-		if (double.Abs(dotProductNormalDirection) < Epsilon)
+		if (double.Abs(normalDotRayDirection) < Epsilon)
 		{
 			// TODO: Remove unnecessary comments!
 			// The line of sight is approximately parallel to the plane.
 			return new List<Intercept>();
 		}
 
-		var distance = normal.Dot(normal - ray.Origin) / dotProductNormalDirection;
+		var distance = normal.Dot(normal - ray.Origin) / normalDotRayDirection;
 
 		// TODO: Can we use new() everywhere?
 		return new()
